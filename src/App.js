ce13a051f8a0d2dce5a253d68cc28216a8f4ec7c
Kyle Liu <kyle116@yahoo.com>
2019-03-29 13:13:39 -0700

    reorg files

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
+// Services
+// Pages
+import HomePage from '../pages/HomePage/HomePage';
+// Stylesheets
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+	<Router>
+		<div className="App">
+			<Route
+				exact path='/'
+				render={(props) => <HomePage {...props} />}
+			/>
+		</div>
+	</Router>
+    );
+  }
+}
+
+export default App;
