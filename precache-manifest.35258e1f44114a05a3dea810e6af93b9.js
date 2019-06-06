3fee5543d0f861d3fe202f535a8d1d33d51098ca
Kyle Liu <kyle116@yahoo.com>
2019-06-06 01:20:25 -0700

    Updates

---
+++ precache-manifest.35258e1f44114a05a3dea810e6af93b9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "d70aff1744b5acf6f527af83f32eb30f",
+    "url": "/instagram-stats/static/media/circle3.d70aff17.png"
+  },
+  {
+    "revision": "527b3ad5bd9cc1a671e8",
+    "url": "/instagram-stats/static/css/main.3e7e6b7f.chunk.css"
+  },
+  {
+    "revision": "0d189221a70ab5896987",
+    "url": "/instagram-stats/static/js/runtime~main.0d189221.js"
+  },
+  {
+    "revision": "65ffa9e800bae4fa61eb3c742aa45de1",
+    "url": "/instagram-stats/static/media/ig3.65ffa9e8.png"
+  },
+  {
+    "revision": "8117e81bb1b52c620496",
+    "url": "/instagram-stats/static/js/2.8117e81b.chunk.js"
+  },
+  {
+    "revision": "b67ed9aafaad7759450f8416f3f179cc",
+    "url": "/instagram-stats/static/media/igmodal.b67ed9aa.png"
+  },
+  {
+    "revision": "527b3ad5bd9cc1a671e8",
+    "url": "/instagram-stats/static/js/main.527b3ad5.chunk.js"
+  },
+  {
+    "revision": "a096b41bf5c75c2a5390ab501287a264",
+    "url": "/instagram-stats/static/media/circle2.a096b41b.png"
+  },
+  {
+    "revision": "e99be06e80b0a4321e21e8749f800a35",
+    "url": "/instagram-stats/static/media/circle1.e99be06e.png"
+  },
+  {
+    "revision": "76926aeb9bc0ef30c7fbfc89c8dbf278",
+    "url": "/instagram-stats/static/media/ig1.76926aeb.png"
+  },
+  {
+    "revision": "3e3a03482e7a90490f8725fd9961482e",
+    "url": "/instagram-stats/static/media/ig2.3e3a0348.png"
+  },
+  {
+    "revision": "8117e81bb1b52c620496",
+    "url": "/instagram-stats/static/css/2.babe449b.chunk.css"
+  },
+  {
+    "revision": "0e6888c6bf52cc7494d464b91d599235",
+    "url": "/instagram-stats/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/instagram-stats/precache-manifest.5ae77a516c8b72818efbd10b5f23468f.js"
+  "/instagram-stats/precache-manifest.35258e1f44114a05a3dea810e6af93b9.js"
 );
 
 workbox.clientsClaim();
